2c07d151f47f51815008a04c4d2c211b0829e383
xayala <13482582+xaviayala@users.noreply.github.com>
2020-02-24 16:07:28 +0100

    refactored 'pages', promoted them from JSX functions to class entities to properly inititialize the multilanguage module

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,57 +2,81 @@ import React from 'react';
 import MainHeader from '../components/MainHeader';
 import { withLocalize, Translate } from "react-localize-redux";
 import Facebook from '../components/Facebook';
+import { renderToStaticMarkup } from "react-dom/server";
+import globalTranslations from "../data/gallery-translations.json";
 
-const AboutPage = () => (
-    <>
-    <MainHeader />
-    <section className="wrapper">
-        <div className="inner">
-            <header className="align-center">
-                <h1><Translate id="pages.about.pageTitle" /></h1>
-                <h3><Translate id="pages.about.warning" /></h3>
-            </header>
-            <div className="flex flex-2">
-                <div className="col col2">
-                    <p>
-                    ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
-                    Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
-                    non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
-                    eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
-                    sodales purus euismod.
-                    </p>
-                    <p>
-                    Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-                    Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-                    nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-                    sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-                    interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-                    consequat quam. Vivamus accumsan dui in facilisis aliquet.
-                    </p>
-                </div>
-                <div className="col col1 first">
-                    <div className="image round fit">
-                        <img src={require(`../assets/images/josep-freixedes.jpg`)} alt="Josep Freixedes" />
+
+class AboutPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.props.initialize({
+            languages: [
+                { name: "English", code: "en" },
+                { name: "Spanish", code: "es" },
+                { name: "Catalan", code: "ca" }
+            ],
+            translation: globalTranslations,
+            options: { 
+                renderToStaticMarkup,
+                defaultLanguage: "en" 
+            }
+        });
+    }
+
+    render() {
+        return (
+             <>
+                <MainHeader />
+                <section className="wrapper">
+                    <div className="inner">
+                        <header className="align-center">
+                            <h1><Translate id="pages.about.pageTitle" /></h1>
+                            <h3><Translate id="pages.about.warning" /></h3>
+                        </header>
+                        <div className="flex flex-2">
+                            <div className="col col2">
+                                <p>
+                                ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+                                Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+                                non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+                                eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+                                sodales purus euismod.
+                                </p>
+                                <p>
+                                Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+                                Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+                                nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+                                sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+                                interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+                                consequat quam. Vivamus accumsan dui in facilisis aliquet.
+                                </p>
+                            </div>
+                            <div className="col col1 first">
+                                <div className="image round fit">
+                                    <img src={require(`../assets/images/josep-freixedes.jpg`)} alt="Josep Freixedes" />
+                                </div>
+                            </div>
+                        </div>
+                        <p>
+                            Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+                            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+                            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+                            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+                            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+                            consequat quam. Vivamus accumsan dui in facilisis aliquet.
+                        </p>
+                        <p> 
+                            Nunc interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+                            consequat quam. Vivamus accumsan dui in facilisis aliquet. nibh elit congue tellus, 
+                            ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan tellus quita.
+                        </p>
+                        <Facebook />
                     </div>
-                </div>
-            </div>
-            <p>
-                    Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-                    Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-                    nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-                    sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-                    interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-                    consequat quam. Vivamus accumsan dui in facilisis aliquet.
-                </p>
-                <p> 
-                    Nunc interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-                    consequat quam. Vivamus accumsan dui in facilisis aliquet. nibh elit congue tellus, 
-                    ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan tellus quita.
-                </p>
-                <Facebook />
-        </div>
-    </section>    
-    </>
-);
+                </section>    
+        </>)
+    }    
+}
 
 export default withLocalize(AboutPage);
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,51 +2,72 @@ import React from 'react';
 import { withLocalize, Translate } from "react-localize-redux";
 import MainHeader from '../components/MainHeader';
 import Facebook from '../components/Facebook';
+import { renderToStaticMarkup } from "react-dom/server";
+import globalTranslations from "../data/gallery-translations.json";
 
-const ContactPage = () => {
+class ContactPage extends React.Component {
 
-    return (
-        <>
-          <MainHeader />
-          <section className="wrapper">
-            <div className="inner">
-                  <header className="align-center">
-                      <h1><Translate id="pages.contact.pageTitle" /></h1>
-                      <h3><Translate id="pages.contact.request" /></h3>
-                  </header>
-                  <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
-                      <input type="hidden" name="form-name" value="contact" />
-                      <div class="field half first">
-                        <label for="name"><Translate id="pages.contact.name" /></label>
-                        <input name="name" id="name" type="text" placeholder="Name" />
-                      </div>
-                      <div class="field half">
-                        <label for="email">Email</label>
-                        <input name="email" id="email" type="email" placeholder="Email" />
-                      </div>
-                      <div class="field">
-                        <label for="message"><Translate id="pages.contact.message" /></label>
-                        <textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
-                      </div>
-                      <div class="field">
-                        <div data-netlify-recaptcha="true"></div>
-                        <div class="g-recaptcha" data-sitekey="6LcnvNsUAAAAADD6_E9geeWuPh5zwE10oOBso-G9"></div>
-                      </div>
-                      <ul class="actions">
-                        <li>
-                            <Translate>{
-                              ({ translate }) => {
-                                return <input value={translate("pages.contact.submit")} class="button" type="submit" />
-                              }
-                            }</Translate>
-                        </li>
-                      </ul>
-                    </form>
-                    <Facebook />
-                </div>
-          </section>
-        </>
-    );
-}
+    constructor(props) {
+        super(props);
+
+        this.props.initialize({
+            languages: [
+                { name: "English", code: "en" },
+                { name: "Spanish", code: "es" },
+                { name: "Catalan", code: "ca" }
+            ],
+            translation: globalTranslations,
+            options: { 
+                renderToStaticMarkup,
+                defaultLanguage: "en" 
+            }
+        });
+    }
+
+    render() {
+        return (
+                <>
+                  <MainHeader />
+                  <section className="wrapper">
+                    <div className="inner">
+                          <header className="align-center">
+                              <h1><Translate id="pages.contact.pageTitle" /></h1>
+                              <h3><Translate id="pages.contact.request" /></h3>
+                          </header>
+                          <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+                              <input type="hidden" name="form-name" value="contact" />
+                              <div className="field half first">
+                                <label htmlFor="name"><Translate id="pages.contact.name" /></label>
+                                <input name="name" id="name" type="text" placeholder="Name" />
+                              </div>
+                              <div className="field half">
+                                <label htmlFor="email">Email</label>
+                                <input name="email" id="email" type="email" placeholder="Email" />
+                              </div>
+                              <div className="field">
+                                <label htmlFor="message"><Translate id="pages.contact.message" /></label>
+                                <textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+                              </div>
+                              <div className="field">
+                                <div data-netlify-recaptcha="true"></div>
+                                <div className="g-recaptcha" data-sitekey="6LcnvNsUAAAAADD6_E9geeWuPh5zwE10oOBso-G9"></div>
+                              </div>
+                              <ul className="actions">
+                                <li>
+                                    <Translate>{
+                                      ({ translate }) => {
+                                        return <input value={translate("pages.contact.submit")} className="button" type="submit" />
+                                      }
+                                    }</Translate>
+                                </li>
+                              </ul>
+                            </form>
+                            <Facebook />
+                        </div>
+                  </section>
+              </>)
+      }    
+  }
+  
 
 export default withLocalize(ContactPage);
