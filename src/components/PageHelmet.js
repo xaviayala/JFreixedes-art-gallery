6f65cca4e07052754224fb969cc99e8cc781d7cb
xayala <13482582+xaviayala@users.noreply.github.com>
2020-04-11 12:52:12 +0200

    Added more metatags on React's helmet component in order to improve SEO scoring

--- src/components/PageHelmet.js
+++ src/components/PageHelmet.js
@@ -5,7 +5,9 @@ const PageHelmet = ({ pageTitle, pageDescription, pageImage}) => {
     return (
         <Helmet>
             <title>{pageTitle}</title>
+            <meta property="og:title" content={pageTitle} />
             <meta name="description" content={pageDescription} />
+            <meta property="og:description" content={pageDescription} />
             <meta name="og:image" content={pageImage} />
         </Helmet>
     );
