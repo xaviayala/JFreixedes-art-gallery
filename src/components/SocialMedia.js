802d707f6f51e6b8b8cdb2221d79cbaf94c142e5
xayala <13482582+xaviayala@users.noreply.github.com>
2020-04-18 14:51:04 +0200

    Fetching new social media icons from an upgraded FA version

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,8 +4,8 @@ const SocialMedia = () => (
     <>
         <h4 className="align-center">
             Follow me on<br/>
-                    <a href="https://www.facebook.com/josep.freixedes" target="_blank" rel="noopener noreferrer" className="facebook"><span className="fa fa-facebook"></span></a> 
-                    <a href="https://twitter.com/ArtFreixedes" target="_blank" rel="noopener noreferrer" className="twitter"><span className="fa fa-twitter"></span></a> 
+                    <a href="https://www.facebook.com/josep.freixedes" target="_blank" rel="noopener noreferrer" className="facebook"><span className="fab fa-facebook-f"></span></a> 
+                    <a href="https://twitter.com/ArtFreixedes" target="_blank" rel="noopener noreferrer" className="twitter"><span className="fab fa-twitter"></span></a> 
                
         </h4>
     </>
