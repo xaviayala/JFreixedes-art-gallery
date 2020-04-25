a38f121ee2b4dceb6e119de4a73ed09223e8362a
xayala <13482582+xaviayala@users.noreply.github.com>
2020-04-25 12:19:01 +0200

    Styling sitemap.xml - added a CSS Bootstrap layer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <footer id="footer">
         
         <div className="copyright">
-            <span className="credits"><Translate id="common.copyright" />&emsp;<a href="cookie-policy" rel="noopener noreferrer"><Translate id="common.cookiePolicyLink" /></a> | <a href="sitemap.xml" rel="noopener noreferrer"><Translate id="common.siteMapLink" /></a></span>
+            <span className="credits"><Translate id="common.copyright" />&emsp;<a href="cookie-policy" rel="noopener noreferrer"><Translate id="common.cookiePolicyLink" /></a> | <a href="/sitemap.xml" rel="noopener noreferrer"><Translate id="common.siteMapLink" /></a></span>
             <div className="logos" >
                 <a href="https://buttercms.com" target="_blank" rel="noopener noreferrer">
                     <img className="logo" src="https://cdn.buttercms.com/JSSDbrHPSnGlLUcyHTn5" alt="ButterCMS" />
